1e62d7df759867bc04f97831df6da82a068bfd19
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-07-30 14:19:10 +0100

    Initialize console app

---
+++ XMLBunny/Program.cs
@@ -0,0 +1,8 @@
+﻿var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+Console.WriteLine("Enter XML File Name: ");
+
+var fileName = Console.ReadLine();
+
+var filePath = path + $"/{fileName}";
+
---
+++ XMLBunny/README.md
@@ -0,0 +1,4 @@
+# XML Bunny
+
+A Console app for analysing the structure of XML files.
+
