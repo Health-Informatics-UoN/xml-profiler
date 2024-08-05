2dbf1f816508bbfdf9852d6b1317a10d30ace47b
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-05 12:47:20 +0100

    Clean up and refactor

--- XMLBunny/Program.cs
+++ XMLBunny/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 using System.Xml;
 using ConsoleTables;
 using XMLBunny;
@@ -7,28 +7,26 @@ var tags = new List<Tag>();
 var values = new List<Value>();
 var ages = new List<int>();
 
+Console.Clear();
+
 void GenerateStatistics()
 { 
     // Get the file path from Desktop
     var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     Console.Write("Enter XML File Name: ");
-    var fileName = Console.ReadLine() ?? "";
+    var fileName = Console.ReadLine() ?? string.Empty;
     var filePath = path + $"/{fileName}";
 
     try
     {
         var doc = new XmlDocument();
         doc.Load(filePath);
-        var root = doc.DocumentElement;
-        
-        ParseXml(root);
+        ParseXml(doc.DocumentElement);
         DisplayData(fileName);
     }
     catch (Exception e)
     {
         Console.WriteLine($"Error: {e.Message}");
-        
-        // Recursive call in the case where there is an error
         GenerateStatistics();
     }
 }
@@ -38,7 +36,15 @@ void ParseXml(XmlNode node)
     if (node == null) return;
     if (node.NodeType == XmlNodeType.Text) return;
     
-    // Check if tag already exists. If it does, increase count, if not create new tag
+    AddOrUpdateTag(node);
+    
+    if (IsTextNode(node)) AddOrUpdateValue(node);
+    
+    foreach (XmlNode child in node.ChildNodes) ParseXml(child);
+}
+
+void AddOrUpdateTag(XmlNode node)
+{
     var tag = tags.Find(x => x.Name == node.Name);
     if (tag == null)
     {
@@ -48,34 +54,34 @@ void ParseXml(XmlNode node)
     {
         ++tag.Count;
     }
-    
-    if (!string.IsNullOrWhiteSpace(node.InnerText) && node.ChildNodes.Count == 1 && node.FirstChild.NodeType == XmlNodeType.Text)
+}
+
+void AddOrUpdateValue(XmlNode node)
+{
+    var tag = tags.Find(x => x.Name == node.Name);
+    var value = values.Find(x => x.Name == node.InnerText);
+    var newValue = new Value { Name = Regex.Replace(node.InnerText.Trim(), @"\s+", " "), Count = 1, Tag = tag };
+    if (value == null)
     {
-        // Check if the value exists and increase if it does. Create new value if it doesn't and attach to a tag
-        var t = tags.Find(x => x.Name == node.Name);
-        var value = values.Find(x => x.Name == node.InnerText);
-        var newValue = new Value { Name = Regex.Replace(node.InnerText.Trim(), @"\s+", " "), Count = 1, Tag = t };
-        if (value == null)
-        {
-            values.Add(newValue);
-        }
-        else
-        {
-            ++value.Count;
-        }
-        if (t != null && !t.Values.Exists(x => x.Name == value?.Name)) t.Values.Add(newValue);
-        
-        // Update the ages
-        if (node.Name.ToLower().Trim() == "age" && int.TryParse(node.InnerText.Trim(), out int age))
-        {
-            ages.Add(age);
-        }
+        values.Add(newValue);
     }
-    
-    foreach (XmlNode child in node.ChildNodes)
+    else
     {
-        ParseXml(child);
+        ++value.Count;
     }
+    if (tag != null && !tag.Values.Exists(x => x.Name == value?.Name)) tag.Values.Add(newValue);
+    
+    if (node.Name.ToLower().Trim() == "age" && int.TryParse(node.InnerText.Trim(), out int age))
+    {
+        ages.Add(age);
+    }
+}
+
+bool IsTextNode(XmlNode node)
+{
+    return !string.IsNullOrWhiteSpace(node.InnerText) &&
+           node.ChildNodes.Count == 1 && 
+           node.FirstChild.NodeType == XmlNodeType.Text;
 }
 
 void DisplayData(string fileName)
@@ -83,15 +89,27 @@ void DisplayData(string fileName)
     Console.Clear();
     Console.WriteLine($"{fileName}: \n");
     
+    DisplayEmtyTags();
+    DisplayValueTags();
+    DisplayAgeRange();
+    
+    Restart();
+}
+
+void DisplayEmtyTags()
+{
     var emptyTags = tags.Where(x => x.Values.Count <= 0).ToList();
     var emptyTagsTable = new ConsoleTable("Tag", "Count");
     foreach (var emptyTag in emptyTags)
     {
         emptyTagsTable.AddRow(emptyTag.Name, emptyTag.Count);
     }
-    emptyTagsTable.Write();
+    emptyTagsTable.Write(Format.Alternative);
     Console.WriteLine();
-    
+}
+
+void DisplayValueTags()
+{
     var valueTags = tags.Where(x => x.Values.Count > 0).ToList();
     foreach (var valueTag in valueTags)
     {
@@ -100,27 +118,33 @@ void DisplayData(string fileName)
         {
             valueTagsTable.AddRow(value.Name, value.Count);
         }
-        valueTagsTable.Write();
+        valueTagsTable.Write(Format.Alternative);
         Console.WriteLine();
     }
-    
-    var ageRangeTable = new ConsoleTable("Age Range");
-    ageRangeTable.AddRow($"{ages.Min()} – {ages.Max()}");
-    ageRangeTable.Write();
-    Console.WriteLine();
-    
-    // Restart after displaying data
-    Restart();
+}
+
+void DisplayAgeRange()
+{
+    if (ages.Any())
+    {
+        var ageRangeTable = new ConsoleTable("Age Range");
+        ageRangeTable.AddRow($"{ages.Min()} – {ages.Max()}");
+        ageRangeTable.Write(Format.Alternative);
+        Console.WriteLine();
+    }
 }
 
 void Restart()
 {
     Console.Write("Do you want to parse another file?: ");
     var response = Console.ReadLine() ?? "";
+    
     if (response.ToLower().Trim() == "yes")
     {
+        Console.Clear();
         GenerateStatistics();
-    } else if (response.ToLower().Trim() == "no")
+    } 
+    else if (response.ToLower().Trim() == "no")
     {
         Environment.Exit(0);
     }
